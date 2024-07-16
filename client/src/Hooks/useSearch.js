76c0f63fca81e72c9f8ac70907b337ffe2e7e283
Campbell Forsyth <camsh69@gmail.com>
2024-07-16 21:12:58 +0100

    Making changes for Vercel

--- client/src/Hooks/useSearch.js
+++ client/src/Hooks/useSearch.js
@@ -10,7 +10,7 @@ export function useSearch() {
   const searchProducts = async (term, page = 1) => {
     setIsLoading(true);
     try {
-      const response = await fetch("api/search", {
+      const response = await fetch("/search", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
